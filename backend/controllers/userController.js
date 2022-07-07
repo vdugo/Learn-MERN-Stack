1c62ab37363f5d171826eb7611f3a5382f8d97fe
unknown <v3rdugo@yahoo.com>
2022-07-06 22:24:07 -0700

    test getMe controller

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -79,7 +79,13 @@ const loginUser = asyncHandler(async (req, res) => {
 // @route GET /api/users/me
 // @access Public
 const getMe = asyncHandler(async (req, res) => {
-    res.json({message: 'User data display'})
+    const { _id, name, email } = await User.findById(req.user.id)
+
+    res.status(200).json({
+        id: _id,
+        name,
+        email
+    })
 })
 
 // generate JWT
