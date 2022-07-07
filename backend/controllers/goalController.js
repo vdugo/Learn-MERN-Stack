e3dabb291f8515c41888f69400586a9925ed06af
unknown <v3rdugo@yahoo.com>
2022-07-06 22:43:04 -0700

    add protection for updateGoal and deleteGoal

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,5 +1,7 @@
 const asyncHandler = require('express-async-handler')
+
 const Goal = require('../models/goalModel')
+const User = require('../models/userModel')
 
 // @desc get goals
 // @route GET /api/goals
@@ -39,6 +41,20 @@ const updateGoal = asyncHandler(async (req, res) => {
         throw new Error('Goal not found')
     }
 
+    const user = await User.findById(req.user.id)
+    // check for user
+    if (!user)
+    {
+        res.status(401)
+        throw new Error('User not found')
+    }
+    // make sure the logged in user matches the goal user
+    if (goal.user.toString() !== user.id)
+    {   
+        res.status(401)
+        throw new Error('User not authorized')
+    }
+
     const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {new: true})
 
     res.status(200).json(updatedGoal)
@@ -54,6 +70,20 @@ const deleteGoal = asyncHandler(async (req, res) => {
     {
         res.status(400)
         throw new Error('Goal not found')
+    }
+
+    const user = await User.findById(req.user.id)
+    // check for user
+    if (!user)
+    {
+        res.status(401)
+        throw new Error('User not found')
+    }
+    // make sure the logged in user matches the goal user
+    if (goal.user.toString() !== user.id)
+    {   
+        res.status(401)
+        throw new Error('User not authorized')
     }
 
     await goal.remove()
